6b92ab143730d15ca7ed81e327971de4be22a7ca
prabin12 <prabin_buth@yahoo.com>
2017-05-29 14:40:26 +0200

    data intensive real time DIRTY

---
+++ res.js
@@ -0,0 +1,11 @@
+//console.log('Hello world');
+
+var http = require("http");
+http.createServer(function(req, res) {
+
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+
+	res.end('Hello World\n');
+}).listen(3000);
+
+console.log('Server running at http://127.0.0.1:3000/');
