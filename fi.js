53561a19ec4abf68ab597b793d334816673c5490
prabin12 <prabin_buth@yahoo.com>
2017-06-05 21:51:31 +0200

    chatrooms

--- fi.js
+++ fi.js
@@ -1,1 +1,6 @@
-console.log("Hello world")
+var http = require('http');
+var server = http.createServer(function(req, res){
+	res.end('Hello Nirvik');
+});
+console.log('Node will respont on localhost:3000');
+server.listen(3000);
